2d135c9f849899b9e87b2261f8d62819629d31f8
Amalgam0616 <uvcat.0000@gmail.com>
2021-11-08 19:35:51 +0900

    UI連携光明見たり

--- Buildrion/Assets/Amalgam/Script/UI/OrderForm.cs
+++ Buildrion/Assets/Amalgam/Script/UI/OrderForm.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OrderForm : MonoBehaviour
 {
     [SerializeField] private OrderRelay orderRelay;
+
+    private OrderInfo orderInfo;
+
+    private Text titleText;
+    private Text rewardText;
+    private Text lowUnitCntText;
+    private Image[] rarityImages;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +24,29 @@ public class OrderForm : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetOrderForm(OrderInfo info, OrderRelay orderR)
+    {
+        orderInfo = info;
+        orderRelay = orderR;
+    }
+
+    //====================================================================
+
+    public void AcceptButton()
+    {
+        if(orderRelay != null)
+        {
+            orderRelay.AcceptOrder(orderInfo.lowRobotCount);
+        }
+    }
+
+    public void CanselButton()
+    {
+        if (orderRelay != null)
+        {
+
+        }
     }
 }
