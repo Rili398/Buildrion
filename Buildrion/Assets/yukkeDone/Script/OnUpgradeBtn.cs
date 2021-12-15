461de78fa560d7be649e0d8f462612bc7e67c2e0
yukkedone <sukkechi@gmail.com>
2021-12-16 00:49:59 +0900

    リザルトUI置き換え、orderboardUI置き換え、強化ボタンアニメーション追加

--- Buildrion/Assets/yukkeDone/Script/OnUpgradeBtn.cs
+++ Buildrion/Assets/yukkeDone/Script/OnUpgradeBtn.cs
@@ -8,12 +8,18 @@ public class OnUpgradeBtn : MonoBehaviour
     public GameObject infoBtnCvs;
     public GameObject infoBtn;
     public GameObject upgradeBtn;
-    
+
+    public Animator InfoAnimator;
+    public Animator UpgradAnimator;
+
+
     public void Onclick()
     {
         infoBtn.GetComponent<Button>().interactable = false;
-        infoBtnCvs.SetActive(false);
-        upgradeBtn.SetActive(true);
+        //infoBtnCvs.SetActive(false);
+        //upgradeBtn.SetActive(true);
+        InfoAnimator.GetComponent<Animator>().SetBool("Open", true);
+        UpgradAnimator.GetComponent<Animator>().SetBool("Open", true);
     }
     
 }
